61ea4231c48900b10ab6d2f3751cefb4291f7ebf
Luciano Taranto <tarantl@amazon.com>
2025-01-23 08:29:36 -0700

    fix: Minor correction to conditional logic for the original packaging flow

--- .projenrc.ts
+++ .projenrc.ts
@@ -175,7 +175,7 @@ packageLanguageTasks.forEach((l) => {
 
     languageTask?.updateStep(0, {
         ...languageTask.steps[0],
-        condition: `node -e "if (!process.env.CI || process.env.GITHUB_JOB.toLowerCase() !== 'build' || process.env.GITHUB_JOB.toLowerCase() !== 'release') process.exit(1)"`
+        condition: `node -e "if (!process.env.CI || (process.env.GITHUB_JOB.toLowerCase() !== 'build' && process.env.GITHUB_JOB.toLowerCase() !== 'release')) process.exit(1)"`
     });
 
     languageTask?.exec(
