0fa4adc195bec8f7f7c6bff35056d25d974bbd84
derrike <26290287+derrike@users.noreply.github.com>
2025-02-12 14:13:40 -0500

    fix: standardize types to strings (#30)
    * fix: standardize types to strings
    
    * update api

--- utilities/generators/index.ts
+++ utilities/generators/index.ts
@@ -1,10 +1,10 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { generateAndInjectAwsManagedRuleEnum } from './lib/aws-managed-rule-groups';
-import { generateAndInjectImageBuilderComponentEnum } from './lib/image-builder-component';
 import { generateAndInjectAwsManagedPolicyClass } from './lib/aws-managed-policy';
+import { generateAndInjectAwsManagedRuleEnum } from './lib/aws-managed-rule-groups';
 import { generateAndInjectEc2InstanceTypes } from './lib/ec2-instance-type';
+import { generateAndInjectImageBuilderComponentEnum } from './lib/image-builder-component';
 import { generateAndInjectSageMakerInstanceTypes } from './lib/sagemaker-notebook-instance-type';
 
 async function main() {
@@ -22,4 +22,7 @@ async function main() {
     }
 }
 
-main();
+main().catch((error) => {
+    console.error('Unhandled error:', error);
+    process.exit(1);
+});
