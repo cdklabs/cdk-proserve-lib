a26e401ff59c778cb99fd63df8fe27e03e86908b
Luciano Taranto <tarantl@amazon.com>
2025-02-17 16:58:42 -0700

    feat: Add new CR to provision DDB tables

---
+++ src/constructs/dynamodb-provision-table/handler/types/table-item.ts
@@ -0,0 +1,7 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Helper alias for items within a DynamoDB table
+ */
+export type TableItem = Record<string, unknown>;
