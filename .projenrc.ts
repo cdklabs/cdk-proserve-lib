a92774b0a69b769b64e6843ce5ca3c2be08fbebb
Derrike Nunn <nunnderr@amazon.com>
2025-02-18 11:32:58 -0600

    enablePRAutomerge

--- .projenrc.ts
+++ .projenrc.ts
@@ -28,9 +28,7 @@ const project = new CdklabsConstructLibrary({
     packageManager: NodePackageManager.YARN_CLASSIC,
     prettier: true,
     prettierOptions: p.prettierOptions,
-    githubOptions: {
-        mergeQueue: true
-    },
+    enablePRAutoMerge: true,
     versionrcOptions: {
         preset: 'conventionalcommits'
     }
