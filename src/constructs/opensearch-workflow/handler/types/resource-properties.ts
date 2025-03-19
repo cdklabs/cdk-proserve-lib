0a5e34d11af50e5f0981c0a7998087a22f2e5eea
Derrike Nunn <nunnderr@amazon.com>
2025-03-19 13:22:44 -0500

    update licenses

--- src/constructs/opensearch-workflow/handler/types/resource-properties.ts
+++ src/constructs/opensearch-workflow/handler/types/resource-properties.ts
@@ -1,11 +1,5 @@
-/**
- * (c) 2025 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
- *
- * This AWS Content is provided subject to the terms of the AWS Customer
- * Agreement available at https://aws.amazon.com/agreement or other written
- * agreement between Customer and either Amazon Web Services, Inc. or
- * Amazon Web Services EMEA SARL or both.
- */
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 import { DestructiveOperation } from '../../../../types/destructive-operation';
 
