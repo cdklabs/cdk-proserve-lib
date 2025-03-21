1e4b4f9c50de2d2b520e798af4fbf656c6bda4f3
Derrike Nunn <nunnderr@amazon.com>
2025-03-21 12:49:44 -0500

    back to no resolutions

--- utilities/projen/lib/deps.ts
+++ utilities/projen/lib/deps.ts
@@ -69,13 +69,6 @@ export const configureDependencies = (project: CdklabsTypeScriptProject) => {
      * Resolutions
      */
     project.package.addPackageResolutions(
-        // 'cross-spawn@^7.0.5', // grype finding nov24
-        'cross-spawn@^7.0.5',
-        'string-width@^4.2.3',
-        'strip-ansi@^6.0.1',
-        'ansi-regex@^5.0.1',
-        'wrap-ansi@^7.0.0'
-        // 'is-fullwidth-code-point@^3.0.0'
-        // 'emoji-regex@^8.0.0'
+        'cross-spawn@^7.0.5' // grype finding nov24
     );
 };
