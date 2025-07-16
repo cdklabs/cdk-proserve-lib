9b504a84f8e560eb2b92c4b5731eeb713385a718
LucianoTaranto <6540591+LucianoTaranto@users.noreply.github.com>
2025-07-16 11:28:35 -0700

    fix: Temporarily disable the UPDATE handler

--- src/constructs/opensearch-provision-domain/handler/on-event/index.ts
+++ src/constructs/opensearch-provision-domain/handler/on-event/index.ts
@@ -138,20 +138,27 @@ async function onUpdate(
     event: CloudFormationCustomResourceUpdateEvent<ResourceProperties>
 ): Promise<CdkCustomResourceResponse<never>> {
     // TODO: Probably doesnt work properly since no access to OLD resource properties
-    const asset = await downloadAndExtractAsset(
-        event.ResourceProperties.AssetS3Uri
-    );
 
-    const config = createProvisionerConfig(event, asset.path);
-    const provisioners = getProvisioners(event, config);
+    return new Promise((resolve) => {
+        resolve({
+            PhysicalResourceId: event.PhysicalResourceId
+        });
+    });
 
-    for (const provisioner of provisioners) {
-        await provisioner.run();
-    }
+    // const asset = await downloadAndExtractAsset(
+    //     event.ResourceProperties.AssetS3Uri
+    // );
 
-    return {
-        PhysicalResourceId: asset.etag
-    };
+    // const config = createProvisionerConfig(event, asset.path);
+    // const provisioners = getProvisioners(event, config);
+
+    // for (const provisioner of provisioners) {
+    //     await provisioner.run();
+    // }
+
+    // return {
+    //     PhysicalResourceId: asset.etag
+    // };
 }
 
 /**
