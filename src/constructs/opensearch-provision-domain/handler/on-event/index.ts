860ba31ffa7d3bee3c3b2f86767b7ab3deb38a89
LucianoTaranto <6540591+LucianoTaranto@users.noreply.github.com>
2025-08-04 12:42:47 -0700

    refactor: Split all utilities into their own files to simplify testing

--- src/constructs/opensearch-provision-domain/handler/on-event/index.ts
+++ src/constructs/opensearch-provision-domain/handler/on-event/index.ts
@@ -9,101 +9,10 @@ import {
     CloudFormationCustomResourceUpdateEvent,
     Context
 } from 'aws-lambda';
-import { AwsHttpClient } from '../../../../common/lambda/aws-http-client';
-import { downloadS3Asset } from '../../../../common/lambda/download-s3-asset';
-import { extractZipFile } from '../../../../common/lambda/extract-zip-file';
-import { ProvisionerConfiguration } from '../types/provisioner-configuration';
 import { ResourceProperties } from '../types/resource-properties';
-import {
-    ClusterSettingsProvisioner,
-    ComponentTemplateProvisioner,
-    IndexProvisioner,
-    IndexTemplateProvisioner,
-    IsmPolicyProvisioner,
-    RoleMappingProvisioner,
-    RoleProvisioner,
-    SavedObjectProvisioner
-} from './utils/provision';
-import { BaseProvisioner } from './utils/provision/base';
-
-/**
- * Creates the configuration to be used by provisioners
- * @param event Input metadata for the custom resource
- * @param assetPath Location of the extracted provisioning configuration assets
- * @returns Provisioner configuration
- */
-function createProvisionerConfig(
-    event: CdkCustomResourceEvent<ResourceProperties>,
-    assetPath: string
-): ProvisionerConfiguration {
-    const client = new AwsHttpClient({
-        service: 'es',
-        baseUrl: `https://${event.ResourceProperties.DomainEndpoint}`,
-        passNonSuccessfulStatusCodes: true,
-        roleArn: event.ResourceProperties.AdminRoleArn,
-        timeout: 45000
-    });
-
-    return {
-        action: event.RequestType,
-        allowDestructiveOperations:
-            event.ResourceProperties.AllowDestructiveOperations,
-        assetPath: assetPath,
-        client: client,
-        domainType: event.ResourceProperties.DomainType
-    };
-}
-
-/**
- * Creates, configures, and orders all provisioners
- * @param event Input metadata for the custom resource
- * @param config Configuration for the provisioners
- * @returns Provisioners
- */
-function getProvisioners(
-    event: CdkCustomResourceEvent<ResourceProperties>,
-    config: ProvisionerConfiguration
-): BaseProvisioner[] {
-    const provisioners: BaseProvisioner[] = [];
-
-    provisioners.push(
-        new ClusterSettingsProvisioner(
-            config,
-            event.ResourceProperties.ClusterSettings
-        )
-    );
-    provisioners.push(new IsmPolicyProvisioner(config));
-    provisioners.push(new ComponentTemplateProvisioner(config));
-    provisioners.push(new IndexTemplateProvisioner(config));
-    provisioners.push(new IndexProvisioner(config));
-    provisioners.push(new RoleProvisioner(config));
-    provisioners.push(
-        new RoleMappingProvisioner(
-            config,
-            event.ResourceProperties.DynamicRoleMappings
-        )
-    );
-    provisioners.push(new SavedObjectProvisioner(config));
-
-    return config.action === 'Delete' ? provisioners.reverse() : provisioners;
-}
-
-/**
- * Downloads and extracts the zip asset containing all the provisioning configuration files
- * @param uri URI to the zip asset in Amazon Simple Storage Service (S3)
- * @returns Etag for the asset downloaded and path on disk to the extracted files
- */
-async function downloadAndExtractAsset(
-    uri: string
-): Promise<{ etag: string; path: string }> {
-    const asset = await downloadS3Asset(uri);
-    const finalPath = extractZipFile(asset.filePath);
-
-    return {
-        etag: asset.etag,
-        path: finalPath
-    };
-}
+import { downloadAndExtractAsset } from './utils/asset';
+import { createProvisionerConfig } from './utils/create-provisioner-config';
+import { getProvisioners } from './utils/get-provisioners';
 
 /**
  * Handles AWS CloudFormation CREATE calls
