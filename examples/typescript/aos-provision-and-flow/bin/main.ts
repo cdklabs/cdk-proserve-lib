ecf521599e132db5e90d9a6a2ea7ec01dbd5c002
Derrike Nunn <nunnderr@amazon.com>
2025-09-03 15:20:50 -0500

    more cleanup

--- examples/typescript/aos-provision-and-flow/bin/main.ts
+++ examples/typescript/aos-provision-and-flow/bin/main.ts
@@ -2,12 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import * as cdk from 'aws-cdk-lib';
-import { AosDomainStack } from '../lib/aos-domain-stack';
 import { AosProvisionAndFlowStack } from '../lib/aos-provision-and-flow-stack';
 
 const app = new cdk.App();
-const aosDomainStack = new AosDomainStack(app, 'AosDomainStack8');
-// new AosProvisionAndFlowStack(app, 'AosProvisionAndFlowStack4', {
-//     domain: aosDomainStack.domain,
-//     domainAdminRole: aosDomainStack.domainAdminRole
-// });
+new AosProvisionAndFlowStack(app, 'AosProvisionAndFlowStack');
